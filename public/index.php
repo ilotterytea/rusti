9ec16f23c9ba96d28a1e5834c0c8b8c5ba29636e
ilotterytea <me@ilotterytea.kz>
2025-03-18 18:32:27 +0500

    feat: frontend

--- public/index.php
+++ public/index.php
@@ -2,10 +2,30 @@
 
 <head>
     <title>tinyi</title>
+    <link rel="stylesheet" href="/static/style.css">
+    <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
-    hail hitler
+    <div class="container">
+        <div class="wrapper">
+            <section class="brand">
+                <img src="/static/img/brand.webp" alt="tinyi">
+            </section>
+
+            <section class="box file-upload">
+                <div class="tab">
+                    <p>File Upload</p>
+                </div>
+                <div class="content">
+                    <form action="/upload.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="file" id="file" required>
+                        <button type="submit">Upload</button>
+                    </form>
+                </div>
+            </section>
+        </div>
+    </div>
 </body>
 
 </html>
