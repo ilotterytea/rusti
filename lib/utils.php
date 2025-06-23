1fa31bab21e893545e32ec71d3c0d651c9a0235a
ilotterytea <me@ilotterytea.kz>
2025-06-24 02:08:40 +0500

    feat: format_timestamp function

--- lib/utils.php
+++ lib/utils.php
@@ -235,3 +235,21 @@ function str_safe(string $s, int|null $max_length, bool $remove_new_lines = true
 
     return $output;
 }
+
+function format_timestamp(int $timestamp_secs)
+{
+    $days = (int) floor($timestamp_secs / (60.0 * 60.0 * 24.0));
+    $hours = (int) floor(round($timestamp_secs / (60 * 60)) % 24);
+    $minutes = (int) floor(round($timestamp_secs % (60 * 60)) / 60);
+    $seconds = (int) floor($timestamp_secs % 60);
+
+    if ($days == 0 && $hours == 0 && $minutes == 0) {
+        return "$seconds second" . ($seconds > 1 ? "s" : "");
+    } else if ($days == 0 && $hours == 0) {
+        return "$minutes minute" . ($minutes > 1 ? "s" : "");
+    } else if ($days == 0) {
+        return "$hours hour" . ($hours > 1 ? "s" : "");
+    } else {
+        return "$days day" . ($days > 1 ? "s" : "");
+    }
+}
