d69179c044d162cd4736c9d72e2ac18ce413729b
ilotterytea <me@ilotterytea.kz>
2025-06-24 09:56:48 +0500

    feat: alerts

---
+++ lib/alert.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'lib/utils.php';
+
+function generate_alert(string $redirect, mixed $data, string|null $message = null, int $code = 200)
+{
+    if ($_SERVER['HTTP_ACCEPT'] == 'application/json') {
+        echo json_response($data, $message, $code);
+    } else {
+        header("Location: {$redirect}?error={$message}&error_status={$code}");
+    }
+}
+
+function html_alert()
+{
+    if (isset($_GET['error'], $_GET['error_status'])) {
+        echo '' ?>
+        <div class="box<?= intval($_GET['error_status'] > 299) ? " red" : "" ?>">
+            <?= str_safe($_GET['error'], null) ?>
+        </div>
+        <?php ;
+    }
+}
