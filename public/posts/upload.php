a51a653776ff60d5fd078023353c28ade1ad4ae1
ilotterytea <me@ilotterytea.kz>
2025-06-26 01:08:48 +0500

    feat: download files from external sources

--- public/posts/upload.php
+++ public/posts/upload.php
@@ -3,6 +3,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../lib/utils.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../lib/image.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../lib/account.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../lib/alert.php';
 
 authorize_user();
 
@@ -14,20 +15,48 @@ if (FILE_AUTHORIZED_UPLOAD && !isset($_SESSION['user'])) {
     exit(json_response(null, 'You must be authorized to upload files', 401));
 }
 
-if (!isset($_FILES['file'])) {
-    exit(json_response(null, 'Field "file" must be set!', 400));
+$file_data = null;
+
+// using yt-dlp to download external content
+if (FILE_EXTSRC && isset($_POST['url'])) {
+    $url = $_POST['url'];
+    $output = [];
+    exec('yt-dlp -f "worst" --get-filename -o "%(filesize_approx)s %(ext)s %(duration)s" "' . escapeshellarg($url) . '"', $output);
+    if (empty($output)) {
+        generate_alert('/', null, 'Bad URL', 400);
+        exit;
+    }
+
+    $output = explode(' ', $output[0]);
+
+    $duration = intval($output[2]);
+    if ($duration == 0 || $duration > FILE_EXTSRC_DURATION) {
+        generate_alert('/', null, sprintf("File must be under %d minutes", FILE_EXTSRC_DURATION / 60), 400);
+        exit;
+    }
+
+    $file_data = [
+        'size' => intval($output[0]),
+        'mime' => ext2mime($output[1]),
+        'extension' => $output[1]
+    ];
+} else if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+
+    $file_data = [
+        'size' => $file['size'],
+        'mime' => $file['type'],
+        'extension' => mime2ext($file_mime)
+    ];
 }
 
-$file = $_FILES['file'];
+if (!$file_data) {
+    generate_alert('/', null, 'URL or file must be sent', 400);
+    exit;
+}
 
-$file_size = $file['size'];
-$file_mime = $file['type'];
-$file_name = pathinfo($file['name'], PATHINFO_FILENAME);
-
-$file_extension = mime2ext($file_mime);
-
-if (!$file_extension) {
-    exit(json_response(null, empty($file_mime) ? 'Corrupted file' : "Unsupported MIME type: {$file_mime}", 400));
+if (!$file_data['extension']) {
+    exit(json_response(null, empty($file_data['mime']) ? 'Corrupted file' : "Unsupported MIME type: {$file_data['mime']}", 400));
 }
 
 $file_id = "";
@@ -39,7 +68,7 @@ do {
         break;
     }
     $file_id = FILE_ID_PREFIX . generate_random_chars(FILE_ID_LENGTH, FILE_ID_CHARPOOL);
-} while (file_exists(FILE_UPLOAD_DIRECTORY . "/{$file_id}.{$file_extension}"));
+} while (file_exists(FILE_UPLOAD_DIRECTORY . "/{$file_id}.{$file_data['extension']}"));
 
 if (empty($file_id)) {
     exit(json_response(null, 'Exceeded time to generate an ID for a file. Try again later.', 500));
@@ -49,7 +78,23 @@ if (!is_dir(FILE_UPLOAD_DIRECTORY . "/uploads") && !mkdir(FILE_UPLOAD_DIRECTORY 
     exit(json_response(null, 'Failed to create upload directory', 500));
 }
 
-if (!move_uploaded_file($file['tmp_name'], FILE_UPLOAD_DIRECTORY . "/uploads/{$file_id}.{$file_extension}")) {
+if (isset($url)) {
+    $result = 0;
+    $output = [];
+    exec(sprintf(
+        'yt-dlp -f "worst" -o "%s/uploads/%s.%s" "%s" 2>&1',
+        FILE_UPLOAD_DIRECTORY,
+        $file_id,
+        $file_data['extension'],
+        escapeshellarg($url)
+    ), $output, $result);
+
+    if ($result != 0) {
+        error_log(sprintf("Failed to download a file (URL: %s): %s", $url, implode('\n', $output)));
+        generate_alert('/', null, 'Failed to download a file! Try again later.', 500);
+        exit;
+    }
+} else if (!move_uploaded_file($file['tmp_name'], FILE_UPLOAD_DIRECTORY . "/uploads/{$file_id}.{$file_data['extension']}")) {
     exit(json_response(null, 'Failed to save a file! Try again later.', 500));
 }
 
@@ -59,12 +104,11 @@ if (!is_dir(FILE_UPLOAD_DIRECTORY . "/thumbnails") && !mkdir(FILE_UPLOAD_DIRECTO
 }
 
 if (
-    isset($file['type']) &&
-    (str_starts_with($file['type'], 'video/') || str_starts_with($file['type'], 'image/')) &&
-    $thumbnail_error = generate_image_thumbnail(FILE_UPLOAD_DIRECTORY . "/uploads/{$file_id}.{$file_extension}[0]", FILE_UPLOAD_DIRECTORY . "/thumbnails/{$file_id}.jpeg", 128, 128)
+    isset($file_data['mime']) &&
+    (str_starts_with($file_data['mime'], 'video/') || str_starts_with($file_data['mime'], 'image/')) &&
+    $thumbnail_error = generate_image_thumbnail(FILE_UPLOAD_DIRECTORY . "/uploads/{$file_id}.{$file_data['extension']}[0]", FILE_UPLOAD_DIRECTORY . "/thumbnails/{$file_id}.jpeg", 128, 128)
 ) {
     exit(json_response(null, "Failed to create a thumbnail (Error code {$thumbnail_error})", 500));
-
 }
 
 $url = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]";
@@ -105,7 +149,7 @@ if (isset($_POST['expires']) && array_key_exists($_POST['expires'], FILE_EXPIRAT
 // saving in database
 $db = new PDO(DB_URL);
 $db->prepare('INSERT INTO posts (id, mime, extension, size, visibility, comment, password, expires_at, uploaded_by) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    ->execute([$file_id, $file_mime, $file_extension, $file_size, $visibility, $comment, $password, $expires_at, $_SESSION['user']['id'] ?? null]);
+    ->execute([$file_id, $file_data['mime'], $file_data['extension'], $file_data['size'], $visibility, $comment, $password, $expires_at, $_SESSION['user']['id'] ?? null]);
 
 // getting data from database
 $stmt = $db->prepare("SELECT * FROM posts WHERE id = ?");
